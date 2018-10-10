ab64fad9e96201389224dc02fd098b788f7ccdc7
Kramerz <Ryan.west@bellevuecollege.edu>
2018-10-10 12:23:51 -0700

    Adding files from in class 10-10-2018

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,12 +1,17 @@
 window.onload = () => {
     const userInput = document.getElementById("list-data");
     const setTextButton = document.getElementById("set-text");
+    const listButton = document.getElementById('add-to-list');
 
     setTextButton.onclick = () => {
         changeOnClick();
     }
-
-
+    
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+    
 }
 
 
