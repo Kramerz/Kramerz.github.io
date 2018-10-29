329982afbc1f4757dcc7feea226dbee03e3b549a
Kramerz <Ryan.west@bellevuecollege.edu>
2018-10-29 11:32:12 -0700

    Working with JavaScript

---
+++ javascript-test/week07-day01.js
@@ -0,0 +1,17 @@
+const num = 3;
+const numString = '3';
+const rain = true;
+
+
+console.log(num, numString);
+console.log(typeof num, typeof numString);
+console.log(num + numString);
+console.log(typeof(num) + numString);
+
+if(rain === true){
+    console.log("looks like it's raining outside, going to go to the gym.");
+}
+else {
+    console.log("Looks like will just go for a walk....");
+}
+
